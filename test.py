7a093eb98741cd4b8a936ad579ba32e9ac45e047
BhavaniPushadapu <113352081+BhavaniPushadapu@users.noreply.github.com>
2023-02-25 11:19:06 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,9 @@
+x=int(input("enter 1st num: "))
+y=int(input("enter 2nd num: "))
+m=int(input("enter weekly salary: "))
+z=int(input())
+a=x/y
+b=m/a
+print(" if " , x,"/",y, "of sam's weekly salary is", m ,". then 1/",z, " of his weekly salary is: ", (1/z)*m*(1/a))
+
 
