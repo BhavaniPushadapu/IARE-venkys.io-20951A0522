2651be9a9927616068d9254edaf8e3c9d74f89de
BhavaniPushadapu <113352081+BhavaniPushadapu@users.noreply.github.com>
2023-03-02 21:38:18 +0530

    Update test.py
    Signed-off-by: BhavaniPushadapu <113352081+BhavaniPushadapu@users.noreply.github.com>

--- test.py
+++ test.py
@@ -1,9 +1,8 @@
-x=int(input("enter 1st num: "))
-y=int(input("enter 2nd num: "))
-m=int(input("enter weekly salary: "))
-z=int(input())
-a=x/y
-b=m/a
-print(" if " , x,"/",y, "of sam's weekly salary is", m ,". then 1/",z, " of his weekly salary is: ", (1/z)*m*(1/a))
-
-
+def reverse(string):
+  l=len(string)
+  if l==0:
+    return
+  else:
+    retrun reverse(string[1:])+string[0]
+string=input("")
+print(reverse(string))
